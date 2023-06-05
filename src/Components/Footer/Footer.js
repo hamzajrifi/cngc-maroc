8ae83334b5992b674332f7545f8830482e71df8f
Hamza Jrifi <hjrifi@e1r6p7.1337.ma>
2023-06-05 11:20:09 +0100

    index page is done

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -5,7 +5,7 @@ export default function Footer() {
     return (
         <footer className='footer'>
             <div className='info-footer'>
-                <div className='logo-nav'>
+                <div className='logo-nav logo-footer'>
                     <a href='#'>
                         <img src='assets/img/logo.png' alt='img-logo-footer-cngc' />
                         <p >CNGC-Maroc</p>
@@ -19,27 +19,45 @@ export default function Footer() {
                     <img src='assets/img/linkden-icon.png' />
                 </div>
             </div>
-            <div className='forClient'>
-                <img className='img-icon-client' src='assets/img/logo.png' />
-                <h1>Pour Le Client</h1>
-                <div>
-                    <img className='icon-address' src='#' />
-                    <p>+212530-010-257</p>
+            <div className='grid-footer'>
+                <div className='forClient'>
+                    <div className='title-client'>
+
+                        <img className='img-icon-client' src='assets/img/icon-business.png' />
+                        <h1>Pour Le Client</h1>
+                    </div>
+                    <div>
+                        <img className='icon-address' src='assets/img/icon-phone.png' />
+                        <p>+212530-010-257</p>
+                    </div>
+                    <div>
+                        <img className='icon-address' src='assets/img/icon-mail.png' />
+                        <p>+212530-010-257</p>
+                    </div>
+                    <div>
+                        <img className='icon-address' src='assets/img/icon-mail.png' />
+                        <p>+212530-010-257</p>
+                    </div>
+                    <div>
+                        <img className='icon-address' src='assets/img/icon-maps.png' />
+                        <p>+212530-010-257</p>
+                    </div>
                 </div>
-                <div>
-                    <img className='icon-address' src='#' />
-                    <p>+212530-010-257</p>
-                </div>
-                <div>
-                    <img className='icon-address' src='#' />
-                    <p>+212530-010-257</p>
-                </div>
-                <div>
-                    <img className='icon-address' src='#' />
-                    <p>+212530-010-257</p>
+                <div className='Horaires'>
+                    <div className='title-client'>
+
+                        <img className='img-icon-client' src='assets/img/horaires.png' />
+                        <h1>Pour Le Client</h1>
+                    </div>
+                    <div className='content-horaire'>
+                        <p className='text-horaires'>Lundi      :       09:00 - 17:00</p>
+                        <p className='text-horaires'>Mardi       :       09:00 - 17:00</p>
+                        <p className='text-horaires'>Mercredi :       09:00 - 17:00</p>
+                        <p className='text-horaires'>Jeudi        :       09:00 - 17:00</p>
+                        <p className='text-horaires'>Vendredi :     09:00 - 12:00/14:00 - 17:00</p>
+                    </div>
                 </div>
             </div>
-            <div></div>
         </footer>
     )
 }
